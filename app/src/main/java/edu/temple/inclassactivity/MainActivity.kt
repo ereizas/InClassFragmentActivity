268dc3461e79d55f6efe686fd352e3fbbdb5bf57
Eric Reizas <tuo17224@temple.edu>
2024-03-14 11:56:48 -0400

    Attached imageDisplayFragment instance to main layout

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
-
+        val imageDisplayFragment = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView,imageDisplayFragment).commit()
     }
 }
